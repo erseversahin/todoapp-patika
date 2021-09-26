7ab940229bbb3b47f6e848c98ae8be569e59e7d8
Şahin ERSEVER <ersversahint@gmail.com>
2021-09-27 00:07:02 +0300

    Footer Copyright Güncellendi

--- View/static/footer.php
+++ View/static/footer.php
@@ -4,5 +4,5 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2021 <a href="<?= url('home') ?>">TodoApp - PhpBasics - Patika</a>.</strong> All rights reserved.
 </footer>
